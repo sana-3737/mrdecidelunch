1a3a9e53cb3ff2b8c9b497cb4713041b28f1b993
sana-3737 <sana.fuwaru@gmail.com>
2026-01-29 10:38:17 +0900

    page+1

---
+++ src/app/login/login.tsx
@@ -0,0 +1,12 @@
+export default function Login() {
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
