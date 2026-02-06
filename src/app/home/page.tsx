8828693eced4fd54d2b79ac7e61b9198a796e27d
ueki-pixel <cfhu1250067@gn.iwasaki.ac.jp>
2026-02-06 09:35:16 +0900

    home作りました

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+        <main>
+            import { Link } from "react-router-dom";
+            <h1>ホーム</h1>
+            <li><Link to="/about">ランチ決定画面へ</Link></li>
+            <li><Link to="/about">会員情報編集画面へ</Link></li>
+        
+        </main>
+    )}
