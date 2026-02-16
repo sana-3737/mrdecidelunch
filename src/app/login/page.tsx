d4efbfde59f73355e86ed9457c733103f1ad3b42
sana-3737 <sana.fuwaru@gmail.com>
2026-02-16 10:00:36 +0900

    change.pagetsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import Link from "next/link"
 import { supabase } from '@/lib/supabase'
 
+
 export default function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
